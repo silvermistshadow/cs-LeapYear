f9b81017cafdb9650f46ebb641e089a48a477b6a
silvermistshadow <bmead72@gmail.com>
2019-12-09 09:19:16 -0800

    initial work

---
+++ LeapYear.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+class LeapYear
+{
+
+
+  static void main() {
+    Console.WriteLine("Enter a year to check if it is a leap year:");
+    int y = Convert.ToInt32(Console.ReadLine());
+  }
+}
